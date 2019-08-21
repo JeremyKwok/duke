2c446370f2ad74f3f5196b49b7f36b6ee64d7aaf
JeremyKwok <jeremykke@gmail.com>
2019-08-21 17:16:54 +0800

    Fixed Bug: Use the same scanner in object throughout. Import scanner lib

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -7,12 +11,12 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         printGreeting();
-        String str = getInput();
+        String str = getInput(in);
         while (!str.equals("bye")) {
             printLinebreak();
             printMessage(str);
             printLinebreak();
-            str = getInput();
+            str = getInput(in);
         }
         printLinebreak();
         printMessage("Bye. Hope to see you again soon!");
@@ -26,8 +30,7 @@ public class Duke {
         printLinebreak();
     }
 
-    private static String getInput() {
-        Scanner in = new Scanner(System.in);
+    private static String getInput(Scanner in) {
         System.out.println();
         return in.nextLine();
     }
