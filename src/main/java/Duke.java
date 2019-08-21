f2764ea207f99258b75015ea8215f242bdf29248
JeremyKwok <jeremykke@gmail.com>
2019-08-21 11:32:51 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,40 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printGreeting();
+        String str = getInput();
+        while (!str.equals("bye")) {
+            printLinebreak();
+            printMessage(str);
+            printLinebreak();
+            str = getInput();
+        }
+        printLinebreak();
+        printMessage("Bye. Hope to see you again soon!");
+        printLinebreak();
     }
+
+    private static void printGreeting() {
+        printLinebreak();
+        printMessage("Hello! I'm Duke");
+        printMessage("What can I do for you?");
+        printLinebreak();
+    }
+
+    private static String getInput() {
+        Scanner in = new Scanner(System.in);
+        System.out.println();
+        return in.nextLine();
+    }
+
+    private static void printLinebreak() {
+        String linebreak = "    ____________________________________________________________";
+        System.out.println(linebreak);
+    }
+
+    private static void printMessage(String str) {
+        System.out.println("     " + str);
+    }
+
+
 }
