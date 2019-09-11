e7774aef70d381fa8b6e1042f300578e362ab9fc
JeremyKwok <jeremykke@gmail.com>
2019-09-11 22:16:26 +0800

    A-TextUiTesting: added progressive test cases

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -5,6 +5,11 @@ if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
 del ACTUAL.TXT
+del ACTUAL-2.TXT
+del ACTUAL-3.TXT
+del ACTUAL-4.TXT
+del ACTUAL-5.TXT
+rm "../../../data/duke.txt"
 
 REM compile the code into the bin folder
 javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\*.java
@@ -16,6 +21,14 @@ REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
+java -classpath ..\bin Duke < input-2.txt > ACTUAL-2.TXT
+java -classpath ..\bin Duke < input-3.txt > ACTUAL-3.TXT
+java -classpath ..\bin Duke < input-4.txt > ACTUAL-4.TXT
+java -classpath ..\bin Duke < input-5.txt > ACTUAL-5.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
+FC ACTUAL-2.TXT EXPECTED-2.TXT
+FC ACTUAL-3.TXT EXPECTED-3.TXT
+FC ACTUAL-4.TXT EXPECTED-4.TXT
+FC ACTUAL-5.TXT EXPECTED-5.TXT
